197a0b344d14d64cfb77bc073a2fcc8b2be3a6f2
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-11-11 13:11:15 +0530

    Rename. Root Package

--- src/main/java/com/springsecurity/SpringsecurityApplication.java
+++ src/main/java/com/springsecurity/SpringsecurityApplication.java
@@ -1,4 +1,4 @@
-package com.springsecurity.springsecurity;
+package com.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
