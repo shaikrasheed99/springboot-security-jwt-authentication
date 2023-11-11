21b4fcbb740c1d68f3644a10a78f0ee493473c66
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-11-11 16:35:06 +0530

    Add. Authorization to signup api

---
+++ src/main/java/com/springsecurity/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf ->
+                csrf.disable()
+        ).authorizeHttpRequests(authorize ->
+                authorize.requestMatchers("/auth/**").permitAll()
+        ).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
